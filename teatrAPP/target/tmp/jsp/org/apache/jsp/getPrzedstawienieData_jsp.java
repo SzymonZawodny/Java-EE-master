6f004a549d23661baa9da8fd325c3c418d55663d
Szymon <zawodny.szymon@gmail.com>
2016-11-05 00:01:40 +0100

    I made changes in przedstawienie.jsp

--- teatrAPP/target/tmp/jsp/org/apache/jsp/getPrzedstawienieData_jsp.java
+++ teatrAPP/target/tmp/jsp/org/apache/jsp/getPrzedstawienieData_jsp.java
@@ -41,34 +41,34 @@ public final class getPrzedstawienieData_jsp extends org.apache.jasper.runtime.H
       _jspx_out = out;
       _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
 
-      out.write("\r\n");
-      out.write("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\r\n");
-      out.write("<html>\r\n");
-      out.write("<head>\r\n");
-      out.write("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\">\r\n");
-      out.write("<link href=\"style.css\" rel=\"stylesheet\" type=\"text/css\">\r\n");
-      out.write("<title>Dodaj Przedstawienie</title>\r\n");
-      out.write("</head>\r\n");
-      out.write(" <style rel=\"stylesheet\" type=\"text/css\">\r\n");
-      out.write(" body {    background-color: #ffcc66; }\r\n");
-      out.write(" .contentbox {\r\n");
-      out.write("    margin-top: 120px;\r\n");
-      out.write("     margin-left: 550px;\r\n");
-      out.write("    width: auto;\r\n");
-      out.write("    height: 100%;\r\n");
-      out.write("    background-color: #ffcc66;\r\n");
-      out.write("    border-color: #A0A0A0;\r\n");
-      out.write("    overflow: auto;\r\n");
-      out.write("    font-family: Arial;   \r\n");
-      out.write("    font-size: 24px; \r\n");
-      out.write("           }\r\n");
-      out.write("    .tak {color:black;  font-weight: bold; }\r\n");
-      out.write("\t.sumbit { text-align: center;}\r\n");
-      out.write("       .tak3 {color:black;  font-weight: bold; text-align: center;}\r\n");
-      out.write("       .stronka { font-weight: bold;  margin-left: 600px; color: black}\r\n");
-      out.write("        </style>\r\n");
-      out.write("<body>\r\n");
-      out.write("\r\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n");
+      out.write("<html>\n");
+      out.write("<head>\n");
+      out.write("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\">\n");
+      out.write("<link href=\"style.css\" rel=\"stylesheet\" type=\"text/css\">\n");
+      out.write("<title>Dodaj Przedstawienie</title>\n");
+      out.write("</head>\n");
+      out.write(" <style rel=\"stylesheet\" type=\"text/css\">\n");
+      out.write(" body {    background-color: #ffcc66; }\n");
+      out.write(" .contentbox {\n");
+      out.write("    margin-top: 120px;\n");
+      out.write("     margin-left: 550px;\n");
+      out.write("    width: auto;\n");
+      out.write("    height: 100%;\n");
+      out.write("    background-color: #ffcc66;\n");
+      out.write("    border-color: #A0A0A0;\n");
+      out.write("    overflow: auto;\n");
+      out.write("    font-family: Arial;\n");
+      out.write("    font-size: 24px;\n");
+      out.write("           }\n");
+      out.write("    .tak {color:black;  font-weight: bold; }\n");
+      out.write("\t.sumbit { text-align: center;}\n");
+      out.write("       .tak3 {color:black;  font-weight: bold; text-align: center;}\n");
+      out.write("       .stronka { font-weight: bold;  margin-left: 600px; color: black}\n");
+      out.write("        </style>\n");
+      out.write("<body>\n");
+      out.write("\n");
       out.write("\t");
       main.java.com.zawodny.domain.Przedstawienie przedstawienie = null;
       synchronized (session) {
@@ -78,11 +78,11 @@ public final class getPrzedstawienieData_jsp extends org.apache.jasper.runtime.H
           _jspx_page_context.setAttribute("przedstawienie", przedstawienie, PageContext.SESSION_SCOPE);
         }
       }
-      out.write("\r\n");
-      out.write("\t");
+      out.write('\n');
+      out.write('	');
       org.apache.jasper.runtime.JspRuntimeLibrary.introspect(_jspx_page_context.findAttribute("przedstawienie"), request);
-      out.write("\r\n");
-      out.write("\t");
+      out.write('\n');
+      out.write('	');
       main.java.com.zawodny.service.PrzedstawienieManager przedstawieniemanager = null;
       synchronized (application) {
         przedstawieniemanager = (main.java.com.zawodny.service.PrzedstawienieManager) _jspx_page_context.getAttribute("przedstawieniemanager", PageContext.APPLICATION_SCOPE);
